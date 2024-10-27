5599ee59563b62c6346700ead7556dd36fdec046
Roland Stedy <stedyroland@gmail.com>
2024-10-27 14:27:24 +0300

    Finished first part of the front end.

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useSnapshot } from 'valtio'
 import state from '../store'
+import { color } from 'framer-motion'
+
+import { getContrastingColor } from '../config/helpers'
 
 const CustomButton = ({type, title,customStyles,handleClick}) => {
   const snap = useSnapshot(state)
@@ -9,7 +12,13 @@ const CustomButton = ({type, title,customStyles,handleClick}) => {
     if (type === 'filled') {
       return{
         backgroundColor: snap.color,
-        color: '#fff'
+        color: getContrastingColor(snap.color)
+      }
+    } else if(type === 'outline'){
+      return{
+        borderWidth: '1px',
+        borderColor: snap.color,
+        color: snap.color
       }
     }
   }
