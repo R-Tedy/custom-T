5aa4201db1c37c66e5e74996d7441034bc91ae9a
Roland Stedy <stedyroland@gmail.com>
2024-10-27 03:14:16 +0300

    customizer skeleton

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,13 @@
 import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
 export {
-  CustomButton
+  CustomButton,
+  ColorPicker,
+  AIPicker,
+  FilePicker,
+  Tab
 }
